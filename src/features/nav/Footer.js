7abd547f02af584840fe5d0f5e3ebfc2a8bdb31a
Christopher Young <christopheryoung@MacBook123.local>
2022-11-07 15:36:09 -0800

    added icon

--- src/features/nav/Footer.js
+++ src/features/nav/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 export default function Footer(props) {
   return (
     <div id="footer">
-      Footer
+      <a href="https://www.flaticon.com/free-icons/vote" title="vote icons" target="_blank" rel="noreferrer">Vote icons created by IconBaandar - Flaticon</a>
     </div>
   );
 }
